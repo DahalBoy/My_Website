f77a5abca534e85cc6720f2fd6b6745054840ffe
Ankit Dahal <122454155+DahalBoy@users.noreply.github.com>
2026-04-10 16:16:22 +0545

    Delete src/src/models/src/services/src/services directory

--- src/src/models/src/services/src/services/src/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-function requireAdminKey(req, res, next) {
-  if (!process.env.ADMIN_API_KEY) return next();
-  const key = req.headers['x-admin-key'];
-  if (!key || key !== process.env.ADMIN_API_KEY)
-    return res.status(401).json({ error: 'Unauthorized.' });
-  next();
-}
-module.exports = { requireAdminKey };
